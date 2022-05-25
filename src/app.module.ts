31636b1ce84ccf8f797bb5b65dfe26d2c2d64578
Junaid Younas <junaid.younas@aquila360.com>
2022-05-25 12:45:31 +0500

    mongoose and config files created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI_LOCAL)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
